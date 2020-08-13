2f2c1dee2722435da653739d0e21c808e5841450
Danial-A <ahmeddanial990@gmail.com>
2020-08-13 15:04:03 +0500

    added keyboard interaction

--- js/script.js
+++ js/script.js
@@ -39,4 +39,28 @@ function toggleBackground() {
 
 
   //clear text function
-  
+  function clear(){
+    if(document.querySelector('.result').innerHTML.contains('.')){
+      return null;
+    }else{
+      return '.';
+    }
+  }
+
+  // window.addEventListener('keydown',(e)=>{
+  //   if(e.key === 1 || e.key === 2 || e.key === 3 || e.key === 4 || e.key === 5 || e.key === 6 || e.key === 7 || e.key === 8 || e.key === 9 || e.key === 0 || e.key === '+'){
+  //     document.querySelector('.userVal').innerHTML += e.key
+  //   }else{
+  //     return;
+  //   }
+  //   console.log(e)
+  // }) 
+  window.addEventListener('keydown',(e)=>{
+    if(e.key === "1" || e.key === "2" || e.key === "3" || e.key === "4" || e.key === "5" || e.key === "6" || e.key === "7" || e.key === "8" || e.key === "9" || e.key === "0" || e.key === '+'){
+      document.querySelector('.userVal').innerHTML += e.key;
+    }
+    else if(e.keyCode === 13){
+      document.querySelector('.result').innerHTML = eval(document.querySelector('.userVal').innerHTML);
+    }
+    console.log(e);
+})
