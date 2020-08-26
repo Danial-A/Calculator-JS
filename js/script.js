1755d0e67cc39037316ccd134dbbb2e9421f207f
Danial-A <ahmeddanial990@gmail.com>
2020-08-26 12:42:10 +0500

    Added delete key function

--- js/script.js
+++ js/script.js
@@ -40,27 +40,34 @@ function toggleBackground() {
 
   //clear text function
   function clear(){
-    if(document.querySelector('.result').innerHTML.contains('.')){
-      return null;
+    if(document.querySelector('.userVal').innerText.contains('.')){
+
+      return console.log('True');
     }else{
-      return '.';
+      return console.log('False');
     }
   }
 
-  // window.addEventListener('keydown',(e)=>{
-  //   if(e.key === 1 || e.key === 2 || e.key === 3 || e.key === 4 || e.key === 5 || e.key === 6 || e.key === 7 || e.key === 8 || e.key === 9 || e.key === 0 || e.key === '+'){
-  //     document.querySelector('.userVal').innerHTML += e.key
-  //   }else{
-  //     return;
-  //   }
-  //   console.log(e)
-  // }) 
+
   window.addEventListener('keydown',(e)=>{
-    if(e.key === "1" || e.key === "2" || e.key === "3" || e.key === "4" || e.key === "5" || e.key === "6" || e.key === "7" || e.key === "8" || e.key === "9" || e.key === "0" || e.key === '+'){
+    if(e.key === "1" || e.key === "2" || e.key === "3" || e.key === "4" || e.key === "5" || e.key === "6" || e.key === "7" || e.key === "8" || e.key === "9" || e.key === "0" || e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/' || e.key === '%'){
       document.querySelector('.userVal').innerHTML += e.key;
     }
     else if(e.keyCode === 13){
       document.querySelector('.result').innerHTML = eval(document.querySelector('.userVal').innerHTML);
     }
-    console.log(e);
+    else if(e.keyCode === 67){
+      document.querySelector('.userVal').innerText = '';
+    }
+    else if(e.keyCode === 8){
+      deleteKey();
+    }
+    // console.log(e);
 })
+
+function deleteKey(){
+  const current = document.querySelector(".userVal").innerText;
+  const newValue = current.slice(0,-1);
+  document.querySelector(".userVal").innerText = newValue;
+  console.log(newValue);
+}
